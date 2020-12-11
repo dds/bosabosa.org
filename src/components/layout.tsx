5f1ba0459cfd0d2f69c7417904376e734a26b8fb
David D. Smith <dsmith@nerdwallet.com>
2020-12-10 23:07:36 -0800

    import * as React from react

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,16 +1,14 @@
-import React from "react"
+import * as React from "react"
 import { Container } from "react-bootstrap"
 import Header from "./header"
 import Footer from "./footer"
 
-const Layout = ({ children }) => {
-  return (
-    <Container fluid className="global-wrapper">
-      <Header />
-      <main>{children}</main>
-      <Footer />
-    </Container>
-  )
-}
+const Layout: React.FC = ({ children }) => (
+  <Container fluid className="global-wrapper">
+    <Header />
+    <main>{children}</main>
+    <Footer />
+  </Container>
+)
 
 export default Layout
