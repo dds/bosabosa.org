407ee7ee2eb7d001e0ac87eb965e19c486fb6243
David D. Smith <davidsmith@acm.org>
2020-12-16 12:45:44 -0800

    get bulma colors working

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -3,8 +3,6 @@ import { bulma } from "@theme-ui/presets"
 export default {
   bulma,
   colors: {
-    text: "#000",
-    primary: "lightblue",
-    background: "#fff",
+    ...bulma.colors,
   },
 }
