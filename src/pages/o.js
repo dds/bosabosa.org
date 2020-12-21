3986c44babb5c95b10302655ccab51584a231cef
David D. Smith <dsmith@nerdwallet.com>
2020-12-21 10:36:10 -0800

    content: start other page

--- src/pages/o.js
+++ src/pages/o.js
@@ -1,5 +1,6 @@
 import React from "react"
-
+import { Container } from "react-bootstrap"
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -7,6 +8,21 @@ const Other = ({ location }) => {
   return (
     <Layout location={location}>
       <SEO title="Other than the blog..." />
+      <Container>
+        <p>
+          Other than <Link to="/b">the blog</Link>, this site is my playground
+          for exploring frontend, design, backend, continuous delivery, and
+          anything else that catches my fancy.
+        </p>
+        <h3>Software</h3>
+        <ul>
+          <li>
+            <Link to="https://github.com/dds/bosabosa.org">
+              bosabosa.org (this site)
+            </Link>
+          </li>
+        </ul>
+      </Container>
     </Layout>
   )
 }
