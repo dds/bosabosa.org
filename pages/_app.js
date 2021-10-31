10d4be172c1e3f7738370f91df460f3a35b5a855
David D. Smith <davidsmith@acm.org>
2021-10-30 19:29:26 -0700

    feat: add facebook-pixel

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,27 @@
 /** @jsxImportSource theme-ui */
-import * as React from "react"
+import { useEffect } from "react"
+import { useRouter } from "next/router"
 import { ThemeProvider } from "theme-ui"
 
 import theme from "../theme"
 import Layout from "../components/layout"
 
 const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    import("react-facebook-pixel")
+      .then(x => x.default)
+      .then(ReactPixel => {
+        ReactPixel.init("374965067696759")
+        ReactPixel.pageView()
+
+        router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView()
+        })
+      })
+  }, [router.events])
+
   return (
     <ThemeProvider theme={theme}>
       <Layout>
